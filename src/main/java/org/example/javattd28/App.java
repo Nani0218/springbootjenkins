a0b2f9d9af83c65a49a45ac04869cc9b94dcfd23
Nani0218 <naninaga196@gmail.com>
2024-10-06 10:11:20 +0530

    commit

---
+++ src/main/java/org/example/javattd28/App.java
@@ -0,0 +1,13 @@
+package org.example.javattd28;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
